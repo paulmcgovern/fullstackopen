634409ca23062fe23e5fa9639015c5284f849b26
Paul McGovern <pmcgovern@pmcgovern.ca>
2023-10-14 14:54:35 -0400

    Exercise 3.12

---
+++ part3/phonebook_backend/mongo.js
@@ -0,0 +1,117 @@
+/*
+
+Set MongoDB connection URL in environment as MONGO_URL. The 
+URL must contain placeholder "__PASSWORD__".
+
+Parameters:
+    Password
+    Name 
+    Number
+
+If only the password is provided, all the entries in the
+phonebook are printed to the console. If the name and 
+number are provided, a new entry in insterted into the DB.
+
+*/
+
+// Max name and number length
+const MAX_LEN = 50
+
+const mongoose = require('mongoose')
+
+require('dotenv').config()
+
+mongoose.set('strictQuery',false)
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+
+// Return a promise with all results.
+const getAll = function () {
+
+    return new Promise((resolve, reject) => {
+
+        Person.find({}).limit(100).then((result) => {
+            resolve(result)
+        })
+    })
+}
+
+// Save a new Person in DB. Return new record.
+const saveNew = function(name, number) {
+
+    if(name.length === 0 || name.length > MAX_LEN) {
+        return new Promise((resolvbe, reject) => {reject("Bad length for name")})
+    }
+
+    if(number.length === 0 || number.length > MAX_LEN) {
+        return new Promise((resolvbe, reject) => {reject("Bad length for number")})
+    }
+
+    return new Promise((resolve, reject) => {
+          
+        const person = new Person({
+            name: name,
+            number: number
+        })
+
+        person.save().then((result) => {           
+            resolve(result)
+        })
+    })
+}  
+
+
+const main = async () => {
+
+    // Args are either password only (3), or password, name,
+    // and number (5)
+    if(process.argv.length != 3 && process.argv.length != 5) {
+        throw new Error("Bad command line parameters.")
+    }
+
+    const password = process.argv[2]
+
+    const mongoUrl = process.env.MONGO_URL.replace("__PASSWORD__", password)
+    
+    await mongoose.connect(mongoUrl).catch(error => {
+        console.log(`Error: ${error.message}`)
+    })
+
+    if(process.argv.length == 3){
+
+        const people = await getAll().catch(erorr => {
+            console.log(`Failed to fetch people ${error.message}`)
+        })
+
+        console.log("Phonebook:")
+
+        people.forEach(person => {
+            console.log(`${person.name} ${person.number}`)
+        })
+
+    } else if(process.argv.length == 5){
+
+        const name = process.argv[3]
+        const number = process.argv[4]
+
+        const person = await saveNew(name, number).catch(error => {
+            console.log(`Failed to create person ${error}`) //.message}`)
+        })
+
+        console.log(`added ${person.name} number ${person.number} to phonebook`)
+
+    } else {   
+        throw Error("Bad number of parameters")
+    }
+
+    await mongoose.connection.close()
+}
+
+main() 
+
